797e6734eed0783b871c621bcbcc87b234bb015c
tt <tomholford0@gmail.com>
2016-02-22 17:55:25 -0800

    finished chapter 5

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -9,7 +9,12 @@ export class HeroService {
   // See the "Take it slow" appendix
   getHeroesSlowly() {
     return new Promise<Hero[]>(resolve =>
-      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
-    );
+      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+      );
+  }
+  getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+      heroes => heroes.filter(hero => hero.id === id)[0]
+      );
   }
 }
